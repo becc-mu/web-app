e6508d904a340e726fb34945388c3c260802adb3
becc-mu <becc.mulugeta@gmail.com>
2019-12-05 17:24:38 +0000

    dynamic style rendering

--- src/components/Greetings.js
+++ src/components/Greetings.js
@@ -4,19 +4,23 @@ function Greetings() {
   const date = new Date()
   const hours = date.getHours()
   let timeOfDay
+  const styles = {
+    fontSize: 30,
+    fontStyle: "italic",
+  }
 
   if (hours < 12) {
     timeOfDay = "morning"
+    styles.color = "rgb(40, 44, 52)"
   } else if (hours >= 12 && hours < 17) {
     timeOfDay = "afternoon"
+    styles.color = "#d90000"
   } else {
     timeOfDay = "night"
+    styles.color = "rgb(46, 9, 39)"
   }
 
-  const styles = {
-    color: "rgb(40, 44, 52)",
-    fontStyle: "italic",
-  }
+  
   return (
   <h2 style={styles}>Good {timeOfDay}!</h2>
   )
