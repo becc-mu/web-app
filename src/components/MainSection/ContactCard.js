97bdd1f370b5976387026cb80a56271e4eebc208
becc-mu <becc.mulugeta@gmail.com>
2019-12-29 22:06:11 +0000

    refactoring image rendering

--- src/components/MainSection/ContactCard.js
+++ src/components/MainSection/ContactCard.js
@@ -1,14 +1,16 @@
 import React from 'react';
-// import './assets/Cat.png';
+
 
 function ContactCard(props) {
   console.log(props)
+
   return(
     <div className="contact-card">
       {/* <img src={require('../assets/Cat.png')} alt=""/> */}
       {/* <img src={Cat} alt=""/> */}
       {/* <img src={props.contact.imgSrc} alt=""/> */}
-      <img src={props.contact.imgUrl} alt={props.contact.name}/>
+     
+      <div className="App" alt={props.contact.name}><img src={props.contact.image} alt={props.contact.name}/></div>
       <h3>{props.contact.name}</h3>
       <p>Phone: {props.contact.phone}</p>
       <p>Email: {props.contact.email}</p>
--- src/components/MainSection/MainContent.js
+++ src/components/MainSection/MainContent.js
@@ -1,51 +1,51 @@
 import React from 'react';
 import Greetings from './Greetings'
 import TodoItem from './TodoItem'
-import ContactCard from './ContactCard';
-import Cat from '../../assets/Cat.png'
-import FunnyCat from '../../assets/funnyCat.png'
-import LookingUp from '../../assets/LookingUp.png'
+import ContactCard from './ContactCard'
+// import Cat from '../assets/Cat.png'
+// import FunnyCat from '../assets/funnyCat.png'
+// import LookingUp from '../assets/LookingUp.png'
 import productsData from './vSchoolProducts'
+import contactsData from './contactsData'
 import Products from './Products'
 import todosData from './todosData'
 
-function MainContent() {
+class MainContent extends React.Component {
+  render() {
   const todosComponent = todosData.map(item => <TodoItem key={item.id} todoItem={item} />)
   const productsComponent = productsData.map(item => <Products key={item.id} product={item}/>)
-  return(
-    <main>
-      <Greetings />
-      <h2>Welcome to my Portfolio page</h2>
-      <p>It is a simple web app I have created using React</p>
-      <h2>Todo List</h2>
-        <div className="todo-list">
-          {todosComponent}
-          {/* <TodoItem />
-          <TodoItem />
-          <TodoItem />
-          <TodoItem /> */}
-        </div>
-        <div>
-          {productsComponent}
-        </div>
-        <div className="contacts">
-          <h2>Cats Contact LIst</h2>
-          <ContactCard
-            contact={{name: "Mr Whiskers", imgUrl: Cat, phone: "(44) 456-1234", email: "m.whiskers@yahoo.com"}}
-          />
-          <ContactCard
-            contact={{name: "Iamsky", imgUrl: FunnyCat, phone: "(44) 456-1234", email: "iamsky@example.com"}}
-          />
-          <ContactCard
-            contact={{name: "Fluffykins", imgUrl: LookingUp, phone: "(44) 356-1534", email: "a.Fluffykins@gmail.com"}}
-          />
-          <ContactCard
-            contact={{name: "Moussky", imgUrl: Cat, phone: "(44) 206-1204", email: "moussky@hotmail.com"}}
-          />
-        </div>
-       
-    </main>
-  )
+  const contactsComponent = contactsData.map(item => <ContactCard key={item.id} contact={item}/>)
+    return(
+      <main>
+        <Greetings />
+        <h2>Welcome to my Portfolio page</h2>
+        <p>It is a simple web app I have created using React</p>
+        <h2>Todo List</h2>
+          <div className="todo-list">
+            {todosComponent}
+          </div>
+          <div>
+            {productsComponent}
+          </div>
+          <div className="contacts">
+            <h2>Cats Contact LIst</h2>
+            {contactsComponent}
+            {/* <ContactCard
+              contact={{name: "Mr Whiskers", imgUrl: Cat, phone: "(44) 456-1234", email: "m.whiskers@yahoo.com"}}
+            />
+            <ContactCard
+              contact={{name: "Iamsky", imgUrl: FunnyCat, phone: "(44) 456-1234", email: "iamsky@example.com"}}
+            />
+            <ContactCard
+              contact={{name: "Fluffykins", imgUrl: LookingUp, phone: "(44) 356-1534", email: "a.Fluffykins@gmail.com"}}
+            />
+            <ContactCard
+              contact={{name: "Moussky", imgUrl: Cat, phone: "(44) 206-1204", email: "moussky@hotmail.com"}}
+            /> */}
+          </div>
+      </main>
+    )
+  }
 }
 
 export default MainContent;
