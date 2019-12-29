233729d834a9a65a92474c1be40c005b724d65aa
becc-mu <becc.mulugeta@gmail.com>
2019-12-29 22:02:03 +0000

    moved assets folder and added contactsData

---
+++ src/components/MainSection/contactsData.js
@@ -0,0 +1,36 @@
+const contactData = [
+  {
+    id: 1,
+    name: "Mr Whiskers",
+    imgUrl: "./assets/Cat.png",
+    image: "./Cat.png",
+    phone: "(44) 456-1234",
+    email: "m.whiskers@yahoo.com",
+},
+{
+    id: 2,
+    name: "Iamsky",
+    imgUrl: "./assets/funnyCat.png",
+    image: "./funnyCat.png",
+    phone: "(44) 456-1234",
+    email: "iamsky@example.com"
+},
+{
+    id: 3,
+    name: "Fluffykins",
+    imgUrl: "./assets/lookingUp.png",
+    image: "./lookingUp.png",
+    phone: "(44) 356-1534",
+    email: "a.Fluffykins@gmail.com"
+},
+{
+    id: 4,
+    name: "Moussky",
+    imgUrl: "./assets/Cat.png",
+    image: "./Cat.png",
+    phone: "(44) 206-1204",
+    email: "moussky@hotmail.com"
+}
+]
+
+export default contactData
